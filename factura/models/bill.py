0bd4a2d2707dfe1ac8fd644bacd534364688de9c
JuanCamilo <juank-963@hotmail.com>
2021-10-22 17:07:33 -0500

    Database migrated with changes

--- factura/models/bill.py
+++ factura/models/bill.py
@@ -1,12 +1,11 @@
 from django.db import models
-#from .user import User
-#from .fact_prod import Fact_prod
-from models import Product, Fact_prod, User
+from .user import User
+from .product import Product
 
 class Bill(models.Model):
     id_bill = models.AutoField(primary_key=True)
     client_name = models.CharField('Client Name', max_length=30)
     purchase_Date = models.DateTimeField()
     isActive = models.BooleanField(default=True)
-    products = models.ManyToManyField(Product, related_name= 'products', through= Fact_prod)
+    products = models.ManyToManyField(Product, related_name= 'products', through= 'Fact_prod')
     user_id = models.ForeignKey(User, related_name='bill', on_delete=models.CASCADE)
