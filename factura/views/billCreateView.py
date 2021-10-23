f1e65f9a9f0ec3f0292a00293610a7a74bbe4275
JuanMayorga <juank-963@hotmail.com>
2021-10-23 16:12:10 -0500

    Adding the posibility to fill another fields in fact_product table 3

--- factura/views/billCreateView.py
+++ factura/views/billCreateView.py
@@ -1,7 +1,7 @@
 from rest_framework import status, views
 from rest_framework.response import Response
 from factura.serializers.billSerializer import BillSerializer
-from factura.models import Bill, Product, User
+from factura.models import Bill, Product, User, Fact_prod
 
 
 class BillCreateView(views.APIView):
@@ -12,7 +12,7 @@ class BillCreateView(views.APIView):
 #        return Response("Added new bill", status=status.HTTP_201_CREATED)
 
 #from django.contrib.auth.models import User
-
+#MyModel.objects.filter(pk=some_value).update(field1='some value')
 #staffprofile.user = user
 
     def post(self, request, *args, **kwargs):
@@ -22,7 +22,7 @@ class BillCreateView(views.APIView):
         new_bill.save()
         for pro in data['products']:
             product_obj = Product.objects.get(product_name = pro['product_name'])
-            
             new_bill.products.add(product_obj)
+            Fact_prod.objects.filter(product_id = product_obj.id_product ).update(product_amount = pro['product_amount'], sub_total_price = product_obj.product_price * pro['product_amount'] )
         serializer = BillSerializer(new_bill)
-        return Response(product_obj.product_price)
+        return Response(serializer.data, product_obj.product_price)
