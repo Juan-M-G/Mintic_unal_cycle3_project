0d1e0c40c4b25c4500e553d2969426e4c44a490d
JuanCamilo <juank-963@hotmail.com>
2021-10-25 17:04:27 -0500

    daily_sells test 5

--- factura/serializers/billSerializer.py
+++ factura/serializers/billSerializer.py
@@ -8,9 +8,11 @@ class BillSerializer(serializers.ModelSerializer):
         fields = ['id_bill', 'client_name', 'purchase_Date', 'isActive', 'products', 'user_id']
     
     def to_representation(self, obj):
+        print(obj)
         bill = Bill.objects.get(id_bill=obj.id_bill)
         user = User.objects.get(id=obj.user_id)
         total_bill = 0
+        print(obj.products)
         for pro in obj.products:
             product_obj = Fact_prod.objects.get(product_id = pro)
             total_bill += product_obj.sub_total_price        
