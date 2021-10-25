1bf2364bc6cc8479be0c97d300f3025e527ca82a
JuanCamilo <juank-963@hotmail.com>
2021-10-25 16:16:36 -0500

    daily_sells test 1

--- factura/serializers/billSerializer.py
+++ factura/serializers/billSerializer.py
@@ -1,4 +1,4 @@
-from factura.models.bill import Bill
+from factura.models import Bill, Fact_prod, User, Product
 from rest_framework import serializers
 
 
@@ -6,4 +6,18 @@ class BillSerializer(serializers.ModelSerializer):
     class Meta:
         model = Bill
         fields = ['id_bill', 'client_name', 'purchase_Date', 'isActive', 'products', 'user_id']
-       
+    
+    def to_representation(self, obj):
+        bill = Bill.objects.get(id_bill=obj.id_bill)
+        user = User.objects.get(id=obj.user_id)
+        total_bill = 0
+        for pro in obj.products:
+            product_obj = Product.objects.get(id_product = pro)
+            total_bill += product_obj.product_price        
+        return {
+            'id_factura': bill.id_bill,
+            'User': user.username,
+            'Client_name': bill.client_name,
+            'purchase_Date': bill.purchase_Date,
+            'total_bill': total_bill
+            }
