dfe66cf479a3ec41c1e8d5c3f869cb33066448f6
JuanCamilo <juank-963@hotmail.com>
2021-10-25 16:36:21 -0500

    daily_sells test 3

--- factura/views/dailySellsDetailView.py
+++ factura/views/dailySellsDetailView.py
@@ -1,13 +1,14 @@
 from rest_framework import generics
-from factura.models import bill
+from rest_framework.response import Response
 from factura.models.bill import Bill
 from factura.serializers.billSerializer import BillSerializer
 from rest_framework.permissions import IsAuthenticated
 
 class DailySellsDetailView(generics.RetrieveAPIView):
-    queryset = Bill.objects.all()
-    serializer_class = BillSerializer(bill, many = True)
     permission_classes = (IsAuthenticated,)
-    
+    def get(self, request, *args, **kwargs):
+        queryset = Bill.objects.all()
+        serializer_class = BillSerializer(queryset, many = True)
+        return Response(serializer_class)
 
 
