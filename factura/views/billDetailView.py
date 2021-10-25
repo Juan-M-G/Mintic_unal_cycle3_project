5c35890fc16ac7228a26508e8424e8b5f8d38086
JuanCamilo <juank-963@hotmail.com>
2021-10-25 11:23:44 -0500

    Added update to User and Product tables

--- factura/views/billDetailView.py
+++ factura/views/billDetailView.py
@@ -1,6 +1,5 @@
 from django.conf import settings
-from rest_framework import generics, status
-#from rest_framework.response import Response
+from rest_framework import generics
 from factura.models.bill import Bill
 from factura.serializers.billSerializer import BillSerializer
 from rest_framework.permissions import IsAuthenticated
